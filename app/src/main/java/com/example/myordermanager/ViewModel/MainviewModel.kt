89dad17a518022b3c8e6efcb5e563d1fdd660521
sidrajawaid <sidra.jawaid00@hotmail.com>
2025-06-23 15:43:26 -0400

    Simple navigation drawer implemented

---
+++ app/src/main/java/com/example/myordermanager/ViewModel/MainviewModel.kt
@@ -0,0 +1,23 @@
+package com.example.myordermanager.ViewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainviewModel  : ViewModel(){
+
+
+   private val _isReady= MutableStateFlow(false)
+    val isReady = _isReady.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(1000L)
+            _isReady.value=true
+        }
+    }
+
+}
--- app/src/main/java/com/example/myordermanager/ui/DashboardScreen.kt
+++ app/src/main/java/com/example/myordermanager/ui/DashboardScreen.kt
@@ -1,34 +1,141 @@
 package com.example.myordermanager.ui
 
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.example.myordermanager.R
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.JsonNull.content
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardScreen() {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
 
-    ModalNavigationDrawer(
-        drawerContent = {
-            ModalDrawerSheet {
-                Text("Drawer title", modifier = Modifier.padding(16.dp))
-                HorizontalDivider()
-                NavigationDrawerItem(
-                    label = { Text(text = "Drawer Item") },
-                    selected = false,
-                    onClick = { /*TODO*/ }
+    /*Surface {
+
+        ModalNavigationDrawer(
+            gesturesEnabled = true,
+            drawerState = drawerState,
+            drawerContent = {
+                ModalDrawerSheet {
+                    Text("Drawer title", modifier = Modifier.padding(16.dp))
+                    HorizontalDivider()
+                    NavigationDrawerItem(
+                        label = { Text(text = "Drawer Item1") },
+                        selected = false,
+                        onClick = {  *//* ToDo *//* })
+
+                    // ...other drawer items
+                }
+            },
+            modifier = TODO(),
+            scrimColor = TODO(),
+            content = TODO()
+        )
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("My App") },
+                    modifier = Modifier.padding(16.dp).background(Color.Gray),
+                    actions = {
+                        IconButton(onClick = { *//* Do something *//* }) {
+                            Icon(Icons.Default.Search, contentDescription = "Search")
+                        }
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            Log.d("<><>", " loggin g")
+                            Toast.makeText(context, "Hello Toast!", Toast.LENGTH_SHORT).show()
+                            scope.launch {
+                                drawerState.open()
+
+                            }
+                        })
+                        {
+                            Icon(Icons.Default.Menu, contentDescription = "drawer")
+                        }
+                    }
                 )
-                // ...other drawer items
+            },
+        )
+        {
+            Column { Modifier.padding(it) }
+        }
+
+    }*/
+
+    Surface(
+    ) {
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+
+        ModalNavigationDrawer(
+            drawerContent = {
+                ModalDrawerSheet {
+
+                        NavigationDrawerItem(
+                            label = { Text(text = "Drawer Item1") },
+                            selected = false,
+                            onClick = {  /*TODO */})}}
+            ,
+            drawerState = drawerState
+        ) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text(text = "Todo App")
+                        },
+                        navigationIcon = {
+                            IconButton(onClick = {
+                                scope.launch {
+                                    drawerState.open()
+                                }
+                            }) {
+                                Icon(
+                                    imageVector = Icons.Default.Menu,
+                                    contentDescription = "Menu"
+                                )
+                            }
+                        }
+                    )
+                }
+            ) {
+Column { Modifier.padding(it) }
             }
         }
-    ) {
-        // Screen content
     }
 }
-
